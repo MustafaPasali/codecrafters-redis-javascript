a220b1091971a0e0fdbcc1ef8bd4be7bc6aea2fc
Mustafa Paşalı <mustafa.pasali@teleskop.app>
2024-03-15 23:50:01 +0300

    Created response to ping message

--- app/main.js
+++ app/main.js
@@ -1,7 +1,11 @@
 const net = require("net");
 
 const server = net.createServer((connection) => {
-  // Handle connection
+  connection.on("data", data => {
+    if (data == "*1\r\n$4\r\nping\r\n") {
+        connection.write("+PONG\r\n")
+    }
+  })
 });
 
 server.listen(6379, "127.0.0.1");
